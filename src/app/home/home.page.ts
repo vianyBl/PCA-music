a7b694d8a7492c1485d4e26b2dfd506bd8a9ce31
vianyBL <viany.brocheroj@pca.edu.co>
2026-01-14 22:54:18 -0500

    Fix: se agrego el slide

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
+
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [ CommonModule, IonHeader, IonToolbar, IonTitle, IonContent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+
 })
+
 export class HomePage {
+  genres = [
+  {
+    title : "musica clasica",
+    img : "assets/musica.jpg",
+    description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book "
+  },
+  {
+
+  }
+  ]
   constructor() {}
 }
