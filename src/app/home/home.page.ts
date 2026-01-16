7129feccd598520b263e1d9732882ae56ebac46f
vianyBL <viany.brocheroj@pca.edu.co>
2026-01-15 19:32:12 -0500

    Agregar 5 slides de géneros musicales con imágenes y descripciones

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -15,14 +15,32 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 export class HomePage {
   genres = [
-  {
-    title : "musica clasica",
-    img : "assets/musica.jpg",
-    description : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book "
-  },
-  {
+    {
+      title: "música clásica",
+      img: "assets/musica.jpg",
+      description: "Sumérgete en la elegancia y belleza de la música clásica, con composiciones inmortales de grandes maestros como Mozart, Beethoven y Bach."
+    },
+    {
+      title: "música moderna",
+      img: "assets/musica-moderna.jpg",
+      description: "Explora los géneros musicales contemporáneos y las tendencias actuales del mercado musical."
+    },
+    {
+      title: "música electrónica",
+      img: "assets/musica-electronica.jpeg",
+      description: "Descubre el mundo de la música electrónica con artistas innovadores y sonidos futuristas."
+    },
+    {
+      title: "música pop - Morat",
+      img: "assets/musica-pop.avif",
+      description: "Cuando el sol se pone y la noche llega, Morat trae melodías que tocan el alma. 'Besos en Guerra', 'Señorita', son historias de amor que nos hacen sentir. La banda que entiende nuestro corazón, que canta nuestros sueños."
+    },
+    {
+      title: "música salsa",
+      img: "assets/musica-salsa.webp",
+      description: "¡Baila al ritmo de la salsa! Siente la pasión, el ritmo y la energía de este icónico género latino que conquista pistas de baile en todo el mundo."
+    }
+  ];
 
-  }
-  ]
   constructor() {}
 }
